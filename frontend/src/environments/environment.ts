a7692222d5cb7bf36d3ec151773aec0bd87ad8e8
endermitas3unidas <endermitas3unidas@gmail.com>
2026-04-12 15:14:17 -0500

    Actualizada URL de API para producción

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://crm-mean-backend.vercel.app/api'  //cambiar después del deploy
+  apiUrl: 'https://crm-backend-sigma-five.vercel.app/api'
 };
