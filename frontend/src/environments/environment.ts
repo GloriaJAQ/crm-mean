80910728bde091d902bfdd97f35d8ad1a4db7575
GloriaJAQ <gloria.jaramillo4084@itse.ac.pa>
2026-04-13 04:07:11 -0500

    Update API URL in environment configuration

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://crm-backend-9vuwbo5dt-gloriajaqs-projects.vercel.app/'
+  apiUrl: 'https://crm-backend-sigma-five.vercel.app/api'
 };
